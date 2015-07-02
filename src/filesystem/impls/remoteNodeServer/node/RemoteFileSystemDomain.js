55fdff6a6b44a191b0f66f5ffb5af2ba486062a3
yofreke <sparkyjoe94@aim.com>
2015-07-01 19:56:07 -0700

    remove any trailing slashes in options.dir.relative for server side visit

--- src/filesystem/impls/remoteNodeServer/node/RemoteFileSystemDomain.js
+++ src/filesystem/impls/remoteNodeServer/node/RemoteFileSystemDomain.js
@@ -180,6 +180,10 @@ function _cmdVisit(path, options, callback) {
     }
     if (options.dir) {
         if (options.dir.path) options.dir.path = new RegExp(options.dir.path);
+        // No trailing slash
+        if (options.dir.relative && options.dir.relative.lastIndexOf('/') === options.dir.relative.length - 1) {
+            options.dir.relative = options.dir.relative.substring(0, options.dir.relative.length - 1);
+        }
     }
 
     var walkOpts = {
